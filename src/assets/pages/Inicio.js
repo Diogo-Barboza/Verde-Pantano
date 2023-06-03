ab30a080c5b27c23281e2452bd75b37aece43788
Diogo-Barboza <diogorodriguesbb@gmail.com>
2023-06-03 13:58:46 -0300

    Carrosel Pronto

--- src/assets/pages/Inicio.js
+++ src/assets/pages/Inicio.js
@@ -1,10 +1,44 @@
+import { useState, useEffect, useRef } from 'react'
+
 import styles from './Inicio.module.css'
 
+import {motion} from 'framer-motion'
+
+import image1 from '../img/foto-carrosel-1.jpg'
+import image2 from '../img/foto-carrosel-2.jpg'
+import image3 from '../img/foto-carrosel-3.jpg'
+import image4 from '../img/foto-carrosel-4.jpg'
+import image5 from '../img/foto-carrosel-5.jpg'
+
+const images = [image1, image2, image3, image4, image5]
+
 function Inicio(){
+    const carrosel = useRef();
+    const [width, setWidth] = useState(0)
+
+    useEffect(() => {
+        console.log(carrosel.current?.scrollWidth, carrosel.current?.offsetWidth)
+        setWidth(carrosel.current?.scrollWidth - carrosel.current?.offsetWidth)
+    }, [])
+
+
     return (
-        <div>
-            <h1>INÍCIO</h1>
-        </div>
+        <section className={styles.inicio_container}>
+            <h1>Bem vindo ao <span>PC Wizard!</span></h1>
+            <p>Descubra o pc para rodar o jogo dos seus sonhos!</p>
+            <motion.div ref={carrosel} className={styles.carrosel} whileTap={{cursor: "grabbing"}}>
+                <motion.div className={styles.inner} drag="x" dragConstraints={{right: 0, left: -width}} initial={{ x: 100}} animate={{ x: 0}} transition={{ duration: 0.8}}>
+
+                    {images.map(images => (
+                        <motion.div  className={styles.item} key={images}>
+                            <img src={images} alt="Imagens Carrosel"/>
+                        </motion.div>
+                    ))}
+
+                </motion.div>
+            </motion.div>
+            <a href='/start'>Start</a>
+        </section>
     )
 }
 
