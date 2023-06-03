991a6171c6b877586bab7bace1566382dd8c42ef
Diogo-Barboza <diogorodriguesbb@gmail.com>
2023-06-03 19:46:39 -0300

    Página Inicial Pronta

--- src/assets/components/LinkButtom.js
+++ src/assets/components/LinkButtom.js
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import styles from './LinkButtom.modules.css'
+import styles from './LinkButtom.module.css'
 
 function LinkButtom({ to, text }){
     return(
