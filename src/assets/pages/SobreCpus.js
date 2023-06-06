15acc8fc072e911007b18fa5b98ce608fe35eff8
Diogo-Barboza <diogorodriguesbb@gmail.com>
2023-06-06 19:53:57 -0300

    Sobre nós (Falta fotos)

--- src/assets/pages/SobreCpus.js
+++ src/assets/pages/SobreCpus.js
@@ -1,6 +1,8 @@
+import styles from './SobreCpus.module.css'
+
 function SobreCpus(){
     return(
-        <div>
+        <div className={styles.container}>
             <h1>Sobre CPUS</h1>
         </div>
     )
