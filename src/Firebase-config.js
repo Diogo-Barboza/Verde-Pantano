ea827d4358006a341f20f9cfcc5a7d3cf649de32
Rafael Kenji <rafak.taira@gmail.com>
2023-06-20 10:29:39 -0300

    adicionando a função de pesquisa com mudanças

---
+++ src/Firebase-config.js
@@ -0,0 +1,15 @@
+const { initializeApp, getAnalytics } = require("firebase/app");
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAK3i1B3kh12ltJdbTZNzVIBTEHyH2ygYo",
+    authDomain: "pcwizard-bdb56.firebaseapp.com",
+    databaseURL: "https://pcwizard-bdb56-default-rtdb.firebaseio.com",
+    projectId: "pcwizard-bdb56",
+    storageBucket: "pcwizard-bdb56.appspot.com",
+    messagingSenderId: "949949117362",
+    appId: "1:949949117362:web:e36e994273e0207394eaab",
+    measurementId: "G-XBEHJ5MD4F"
+  };
+  
+  module.exports = firebaseConfig;
+  
